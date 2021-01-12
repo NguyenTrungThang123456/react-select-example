df9c0cd5ef6b5600a3d5b81cd221069b7bc9662d
trungthang1610 <thangnt.nde18074@vtc.edu.vn>
2021-01-12 16:26:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import Select from 'react-select';
+import Creatable from 'react-select/creatable';
+import AsyncSelect from 'react-select/async';
 
 function App() {
+  const aquaticCreatures = [
+    { label: 'Shark', value: 'Shark' },
+    { label: 'Dolphin', value: 'Dolphin' },
+    { label: 'Whale', value: 'Whale' },
+    { label: 'Octopus', value: 'Octopus' },
+    { label: 'Crab', value: 'Crab' },
+    { label: 'Lobster', value: 'Lobster' },
+  ];
+  const loadOptions = (inputValue, callback) => {
+    // perform a request
+    console.log(inputValue);
+    const requestResults = aquaticCreatures.filter(
+      (aquaticCreature) =>
+        aquaticCreature.label.toLowerCase().indexOf(inputValue.toLowerCase()) >
+        -1
+    );
+
+    callback(requestResults);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* <Select
+        options={aquaticCreatures}
+        isMulti
+        onChange={(opt) => console.log('opt', opt)}
+        defaultValue={aquaticCreatures.lastIndex}
+      /> */}
+      {/* <Creatable
+        options={aquaticCreatures}
+        onChange={(opt, meta) => console.log(opt, meta)}
+      /> */}
+      <AsyncSelect
+        loadOptions={loadOptions}
+        isMulti
+        // options={aquaticCreatures}
+        // defaultOptions={aquaticCreatures.lastItem}
+      />
     </div>
   );
 }
